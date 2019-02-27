b8a98d7239366aa38b127b5c3de8db7b4a33ce62
TrishaPW <40293344+TrishaPW@users.noreply.github.com>
2019-02-27 09:52:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Bot aktif!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
+    if (message.content === '!yaz') {
+    	message.delete()
+        const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setDescription(message.author.username + " says: " + args.join(" "));
+		message.channel.send({embed})
   	}
 });
 
 client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
+    if (message.content === 'amk') {
+    	message.reply('küfür etmek çok yanlış bir davranış, sana hiç yakışmadı.');
   	}
 });
 
