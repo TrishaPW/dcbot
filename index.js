0fe5e9bf418c839fbaa0dec31fcdca34d87a3dd4
TrishaPW <40293344+TrishaPW@users.noreply.github.com>
2019-02-27 09:25:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const botconfig = require("./package.json");
 const Discord = require("discord.js");
-bot.commands = new Discord.Collection();
 
 const bot = new Discord.Client({disableEveryone: true});
 
@@ -9,5 +8,32 @@ bot.on("ready", async () => {
 
   bot.user.setActivity("19ROLEPLAY", {type: "WATCHING"});
 });
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**[!]** Bu komutu kullanmak için yetkiniz bulunmamaktadır.");
+  if(!args[0]) return message.channel.send("no");
+  message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send(`${args[0]} adet mesaj silindi.`).then(msg => msg.delete(2000));
+});
+
+}
+
+module.exports.help = {
+  name: "temizle"
+}
+
+module.exports.run = async (bot, message, args) => {
+
+      if(!message.member.hasPermission("ADMINISTRATOR")) return;
+      const sayMessage = args.join(" ");
+      message.delete().catch();
+      message.channel.send(sayMessage);
+
+}
+
+module.exports.help = {
+  name: "yaz"
+}
 
 bot.login(process.env.BOT_TOKEN);
